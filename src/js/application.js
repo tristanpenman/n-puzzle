bb388e5031b18314cba063946ee0d30997b3a453
Tristan Penman <tristan@tristanpenman.com>
2014-03-03 17:38:51 +1100

    Change default goal state to match Java applet.

--- src/js/application.js
+++ src/js/application.js
@@ -5,7 +5,7 @@ $(function() {
     // Set up the default configuration
     var configuration = new Configuration();
     configuration.getInitialState().setTiles([0,2,3,1,4,5,8,7,6]);
-    configuration.getGoalState().setTiles([1,2,3,4,5,0,8,7,6]);
+    configuration.getGoalState().setTiles([1,2,3,8,0,4,7,6,5]);
 
     // Initialise the application state with the default configuration
     var applicationState = new ApplicationState({
