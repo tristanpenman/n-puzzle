dc9297faa1f0dbf8134cf39790e46aa3b013e262
Tristan Penman <tristan@tristanpenman.com>
2021-04-19 21:22:44 +1000

    Use vue.js for PuzzleStateView

---
+++ src/js/components/puzzle-state-view.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="PuzzleStateView">
+    <div class="row">
+      <div class="cell">{{ tiles[0] || '-' }}</div>
+      <div class="cell">{{ tiles[1] || '-' }}</div>
+      <div class="cell">{{ tiles[2] || '-' }}</div>
+    </div>
+    <div class="row">
+      <div class="cell">{{ tiles[3] || '-' }}</div>
+      <div class="cell">{{ tiles[4] || '-' }}</div>
+      <div class="cell">{{ tiles[5] || '-' }}</div>
+    </div>
+    <div class="row">
+      <div class="cell">{{ tiles[6] || '-' }}</div>
+      <div class="cell">{{ tiles[7] || '-' }}</div>
+      <div class="cell">{{ tiles[8] || '-' }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: [
+    'tiles',
+  ]
+};
+</script>
+
+<style>
+.PuzzleStateView {
+  background: #999;
+  border: 1px solid #999;
+  border-radius: 0.3em;
+  display: inline-block;
+  font-size: 12px;
+  margin-bottom: 0.5em;
+}
+
+.PuzzleStateView > .row {
+  display: flex;
+  margin-bottom: 1px;
+}
+
+.PuzzleStateView > .row:last-child {
+  margin-bottom: 0;
+}
+
+.PuzzleStateView > .row > .cell {
+  align-items: center;
+  background: #fff;
+  display: inline-flex;
+  height: 29px;
+  justify-content: center;
+  margin-right: 1px;
+  width: 29px;
+}
+
+.PuzzleStateView > .row > .cell:last-child {
+  margin-right: 0;
+}
+
+.PuzzleStateView > .row:first-child > .cell:first-child {
+  border-top-left-radius: 3px;
+}
+
+.PuzzleStateView > .row:first-child > .cell:last-child {
+  border-top-right-radius: 3px;
+}
+
+.PuzzleStateView > .row:last-child > .cell:first-child {
+  border-bottom-left-radius: 3px;
+}
+
+.PuzzleStateView > .row:last-child > .cell:last-child {
+  border-bottom-right-radius: 3px;
+}
+</style>
--- src/js/views/PuzzleStateView.js
+++ src/js/views/PuzzleStateView.js
@@ -1,24 +1,18 @@
 PuzzleStateView = Backbone.View.extend({
+  initialize: function () {
+    this.render();
+    this.model.on('change', this.render, this);
+  },
 
-    initialize: function() {
-        this.render();
-        this.model.on('change', this.render, this);
-    },
-
-    render: function() {
-        var cells = this.$el.find('.cell');
-        var cell_height = cells.height();
-        var cell_values = cells.find('.cell_value');
-        cell_values.css('top', Math.round((cell_height - cell_values.height()) / 2) + "px");
-        for (var y = 0; y < 3; y++) {
-            for (var x = 0; x < 3; x++) {
-                var tile = this.model.getTile(x, y);
-                if (tile == 0) {
-                    tile = '\u2012';
-                }
-                this.$el.find('.cell.cell_' + x + '_' + y + ' .cell_value').html(tile);
-            }
-        }
-    }
-
+  render: function () {
+    new Vue({
+      el: this.el,
+      data: {
+        model: this.model
+      },
+      template: `
+        <puzzle-state-view v-bind:tiles="model.valueOf()"></puzzle-state-view>
+      `
+    });
+  }
 });
