7ac364ec37367481bb6b77ce7518e52428d2dc99
Tristan Penman <tristan@tristanpenman.com>
2021-04-30 16:11:19 +1000

    Begin modernising PuzzleStateRenderer.js and Util.js

--- src/js/helpers/Util.js
+++ src/js/helpers/Util.js
@@ -1,5 +1,9 @@
 Util = {
-    isInteger: function(n) {
-        return typeof(n) == 'number' && parseInt(n) == n;
-    }
+  isInteger: function (n) {
+    return typeof (n) == 'number' && parseInt(n) === n;
+  },
+
+  log10: function (val) {
+    return Math.log(val) / Math.LN10;
+  }
 };
