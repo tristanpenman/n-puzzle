58593a2a836826b119adaaa44eb9c0ada22989ff
Tristan Penman <tristan@tristanpenman.com>
2026-07-21 09:14:09 +1000

    Add GitHub action to deploy master

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)))
 
 export default defineConfig({
   root: "src",
+  base: "./",
   plugins: [vue()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version)
